cf57566c88d87cfe2fee0fc43ecc1f1872587c35
jikstra <jikstra@disroot.org>
2020-06-30 21:23:42 +0200

    Improve error handling and log output

--- midi.py
+++ midi.py
@@ -6,42 +6,48 @@ import sys
 
 SLEEP_INTERVAL = 0.3
 
-#with mido.open_input('MiniX1', client_name='MiniX1', virtual=True) as input:
-#    with mido.open_output('MiniX1', client_name='MiniX1', virtual=True) as output:
-#        while True:
-#            for msg in input.iter_pending():
-#                output.send(msg)
+def printException(e):
+    print("Error: ", e)
+    print(traceback.format_exc())
+    print(sys.exc_info()[0])
+    time.sleep(SLEEP_INTERVAL)
 
-device = '/dev/ttyACM0'
-with mido.open_output('MiniX1', client_name='MiniX1', virtual=True) as output:
-    while True:
-        try:
-            connection = None
+def main():
+    device = '/dev/ttyACM0'
+    with mido.open_output('MiniX1', client_name='MiniX1', virtual=True) as output:
+        found_device = True
+        while True:
             try:
-                connection = serial.Serial(device, 115200, timeout=1, exclusive=True)
-            except Exception as e:
-                print("Error: ", e)
-                print(traceback.format_exc())
-                print(sys.exc_info()[0])
-                time.sleep(SLEEP_INTERVAL)
-                connection.close()
+                connection = None
+                try:
+                    connection = serial.Serial(device, 115200, timeout=1, exclusive=True)
+                except serial.SerialException as e:
+                    if found_device == True:
+                        print("Could not find device ", device)
+                        found_device = False
 
-            try:
-                while True:
-                    buf = connection.read(3)
-                    if len(buf) != 3:
-                        continue
-                    message = mido.parse(buf)
-                    print(message)
-                    output.send(message)
+                    continue
+                except Exception as e:
+                    printException(e)
+                    connection.close()
+
+                print("Opened device", device)
+                found_device = True
+
+                try:
+                    while True:
+                        buf = connection.read(3)
+                        if len(buf) != 3:
+                            continue
+                        message = mido.parse(buf)
+                        print(message)
+                        output.send(message)
+                except Exception as e:
+                    printException(e)
+                    connection.close()
             except Exception as e:
-                print("Error: ", e)
-                print(traceback.format_exc())
-                print(sys.exc_info()[0])
-                time.sleep(SLEEP_INTERVAL)
-                connection.close()
-        except Exception as e:
-            print("Error: ", e)
-            print(traceback.format_exc())
-            print(sys.exc_info()[0])
-            time.sleep(SLEEP_INTERVAL)
+                printException(e)
+
+
+if __name__ == '__main__':
+    main()
